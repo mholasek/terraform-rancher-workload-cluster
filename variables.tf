30bf7cac6d901970935770e690de2eef4bfb5e00
Michael Holasek <michael.holasekl@bitmedia.at>
2020-04-29 17:24:09 +0200

    add aws_keys to variables

--- variables.tf
+++ variables.tf
@@ -1,3 +1,13 @@
+variable "aws_access_key" {
+  type        = string
+  description = "AWS access key used to create infrastructure"
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS secret key used to create AWS infrastructure"
+}
+
 variable "name" {
   description = "Name to identify Kubernetes cluster"
   type        = string
